aa4ac3033129a37934c991297f013a5e554b232e
Jonathan Shek <jonathan.shek@leanplum.com>
2019-02-28 19:24:40 -0800

    finished weather app

--- node/weather-app/app-promise.js
+++ node/weather-app/app-promise.js
@@ -19,8 +19,26 @@ var geocodeUrl = 'http://www.mapquestapi.com/geocoding/v1/address?key=tLaGSiL2xD
 
 // axios.get returns a Promise
 axios.get(geocodeUrl).then((response) => {
-	console.log(response.data);
+	const loc = response.data.results[0].locations[0]
+
+	if (response.data.status === 'ZERO RESULTS') {
+		throw new Error('Unable to find that address.')
+	}
+
+	var lat = loc.latLng.lat;
+	var lng = loc.latLng.lng;
+	var weatherURL = 'https://api.forecast.io/forecast/05269da3c6d483e814570d6cb46c67fb/' + lat + ',' + lng
+	console.log(loc.street);
+	return axios.get(weatherURL);
+}).then((response) => {
+	var temperature = response.data.currently.temperature;
+	var apparentTemperature = response.data.currently.apparentTemperature;
+	console.log('It\'s currently ' + temperature + '. It feels like ' + apparentTemperature + '.');
 }).catch((e) => {
-	if (e.code === )
-	console.log(e)
+	// temp error catcher
+	if (loc.street === "") {
+		console.log('Unable to connect to API servers.')
+	} else {
+		console.log(e.message);
+	}
 });
