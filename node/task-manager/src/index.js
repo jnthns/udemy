3d421cf2312a01370b62e9e3f0a285aa2d231e24
Jonathan Shek <jonathan.shek@leanplum.com>
2019-03-22 15:19:22 -0700

    finished section

--- node/task-manager/src/index.js
+++ node/task-manager/src/index.js
@@ -12,6 +12,45 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+
+
+
+// // Multer Example
+// const multer = require('multer')
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         if (!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error('Please upload a Word document'))
+//         }
+
+//         cb(undefined,true)
+//         // cb(new Error('File must be a PDF'))
+//         // cb(undefined, true)
+//         // cb(undefined, false)
+//     }
+// })
+
+// app.post('/upload', upload.single('upload'), (req,res) => {
+//     res.send('File uploaded.')
+// }, (error, req, res, next) => { // returns a JSON error instead of HTML 
+//     res.status(400).send({ error: error.message })
+// })
+
 // middleware example 
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
@@ -21,23 +60,12 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
 // Example of Router
 // const router = new express.Router()
 // router.get('/test', (req,res) => {
 //     res.send('This is from my other router.')
 // })
 // app.use(router)
-
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
-
-const Task = require('./models/task')
-const User = require('./models/user')
 
 // const main = async () => {
 //     // const task = await Task.findById('5c941d4e72fca166d2b06d2a')
